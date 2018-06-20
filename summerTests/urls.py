d27229b219ef3b373a057abe881a51a35e1dca8b
Aadway <aadwaysinha@gmail.com>
2018-06-20 21:42:27 +0530

    Views and urls

---
+++ summerTests/urls.py
@@ -0,0 +1,27 @@
+"""summerTests URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),  # We don't want to make this messy. Therefore we create a whole new
+                                                   # urls.py file for our apps and include it over here.
+                                                   # whenever there is a call starting with a particular app name,
+                                                   # it is directed from here to it's own urls.py file
+]
