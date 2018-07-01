7f884a1310bcf4c530a59ee0aa77b6b16071997e
Aadway <aadwaysinha@gmail.com>
2018-07-02 01:33:31 +0530

    login redirect on homepage

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', login, {'template_name': 'account/login.html'}),
+    url(r'^logout/$', logout, {'template_name': 'account/logout.html'}),
 ]
--- summerTests/urls.py
+++ summerTests/urls.py
@@ -1,24 +1,10 @@
-"""summerTests URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf.urls import url, include
 from django.contrib import admin
+from summerTests.views import login_redirect
 
 
 urlpatterns = [
+    url(r'^$', login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),  # We don't want to make this messy. Therefore we create a whole new
                                                    # urls.py file for our apps and include it over here.
