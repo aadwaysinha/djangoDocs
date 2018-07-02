878196cb1bda6925ed89e2d85627216f7f4668b7
Aadway <aadwaysinha@gmail.com>
2018-07-03 00:36:25 +0530

    registration form added

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.views import login, logout
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', login, {'template_name': 'account/login.html'}),
+    url(r'^register/$', views.register, name='reg_form'),
     url(r'^logout/$', logout, {'template_name': 'account/logout.html'}),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 def home(request):
@@ -6,3 +7,14 @@ def home(request):
     args = {'name': name}
     return render(request, 'account/home.html', args)
 
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = UserCreationForm()
+        args = {'form': form}
+        return render(request, 'account/reg_form.html', args)
